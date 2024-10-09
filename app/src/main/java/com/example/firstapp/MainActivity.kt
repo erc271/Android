f73db9dcbec529de281b2e51af1816ce9091f2ea
Ayoub <ayoubsolicode07@gmail.com>
2024-10-09 10:00:10 +0100

    hey

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.firstapp.ui.theme.FirstAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,11 +34,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!!",
-        modifier = modifier,
-        color = color.red,
-    )
+    // Set the background color of the Surface to Blue
+    Surface(
+        color = Color.Gray,
+        modifier = modifier
+    ) {
+        Text(
+            text = "Hey My Name is $name!!",
+            // Set the text color to Red
+            color = Color.Yellow,
+            modifier = modifier.padding(10.dp)
+
+        )
+    }
 }
 
 @Preview(showBackground = true)
